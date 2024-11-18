382dd668fbffa7dc7f0c50eb8e4f9cdbe81822ce
Sanjaya Gayan <k.m.s.g.rathnayaka@gmail.com>
2024-11-19 01:38:08 +0530

    Updated Appointments Page

--- modules/appointments/Appointments.tsx
+++ modules/appointments/Appointments.tsx
@@ -7,7 +7,13 @@ type Props = {
 };
 const Appointments = ({ appointments }: Props) => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen ">
+       <div>
+        <h1 className="text-[20px] sm:text-[32px] font-bold text-primary-color py-3 px-8 sm:px-10 sm:py-10">
+          My Appointments
+        </h1>
+      </div>
+      <div className="px-8 sm:px-10">
         <Grid
       container
       spacing={{ xs: 2, md: 3 }}
@@ -20,7 +26,10 @@ const Appointments = ({ appointments }: Props) => {
       ))}
     </Grid>
     </div>
+    </div>
     
   );
 };
+
+
 export default Appointments;
